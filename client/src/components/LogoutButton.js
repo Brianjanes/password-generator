a86841474747a5c036cc0a74febfe2eb6af6bc59
Brian Janes <janes.brian@gmail.com>
2023-08-25 12:54:41 -0230

    auth0 actually working :-)

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -2,17 +2,9 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+  const { logout, isAuthenticated } = useAuth0();
 
-  return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
-    >
-      Logout
-    </button>
-  );
+  return isAuthenticated && <button onClick={() => logout()}>Logout</button>;
 };
 
 export default LogoutButton;
--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -2,12 +2,23 @@ import { Container } from "@mui/material";
 import React from "react";
 import LoginButton from "../components/LoginButton";
 import LogoutButton from "../components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Login = () => {
+  const { user } = useAuth0();
+
   return (
     <Container maxWidth="sm">
       <LoginButton />
       <LogoutButton />
+      <div>
+        {user && (
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </>
+        )}
+      </div>
     </Container>
   );
 };
